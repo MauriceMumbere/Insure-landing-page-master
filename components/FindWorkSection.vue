980bc190f4e3c612eea4af65d4e0932a88007ff9
maurice Mumbere <mauricendayango@gmail.com>
2025-06-11 10:17:40 +0100

    feat(FindWorkSection): create component

---
+++ components/FindWorkSection.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="py-[7rem] px-5 md:px-[12rem] text-white">
+    <div
+      class="flex flex-col space-y-6 md:flex-row justify-between items-center text-center md:text-left p-8 md:p-[4.5rem] bg-blue-950 bg-[url('./images/bg-pattern-how-we-work-mobile.svg')] md:bg-[url('./images/bg-pattern-how-we-work-desktop.svg')] bg-no-repeat bg-right"
+    >
+      <h2 class="text-4xl md:text-5xl max-w-[27rem]">
+        Find out more about how we work
+      </h2>
+      <div>
+        <a
+          class="border-2 border-white border-solid hover:bg-white hover:text-black py-2 px-7"
+          href="#"
+          >How we work</a
+        >
+      </div>
+    </div>
+  </section>
+</template>
