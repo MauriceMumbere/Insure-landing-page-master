1b0312761a32fd76a76defc5609bb3744307c669
maurice Mumbere <mauricendayango@gmail.com>
2025-06-11 10:19:22 +0100

    feat(OptionSection): create component

---
+++ components/OptionSection.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="pt-[22rem] px-5 md:px-[12rem]">
+    <div>
+      <h2
+        class="text-5xl md:text-7xl text-center md:text-left font-bold before:content-[''] before:absolute before:top-[-5rem] before:h-1 before:w-40 before:bg-black max-w-[34rem] relative text-blue-950"
+      >
+        We’re different
+      </h2>
+      <div
+        class="grid grid-cols-1 text-center md:text-left md:grid-cols-3 gap-[3.2rem] pt-[5rem]"
+      >
+        <div>
+          <div class="flex justify-center md:justify-start">
+            <img src="../public/images/icon-snappy-process.svg" alt="" />
+          </div>
+          <h3 class="pt-8 pb-5 font-bold text-2xl text-blue-950">
+            Snappy Process
+          </h3>
+          <p>
+            Our application process can be completed in minutes, not hours.
+            Don’t get stuck filling in tedious forms.
+          </p>
+        </div>
+        <div>
+          <div class="flex justify-center md:justify-start">
+            <img src="../public/images/icon-affordable-prices.svg" alt="" />
+          </div>
+          <h3 class="pt-8 pb-5 font-bold text-2xl text-blue-950">
+            Affordable Prices
+          </h3>
+          <p>
+            We don’t want you worrying about high monthly costs. Our prices may
+            be low, but we still offer the best coverage possible.
+          </p>
+        </div>
+        <div>
+          <div class="flex justify-center md:justify-start">
+            <img src="../public/images/icon-people-first.svg" alt="" />
+          </div>
+          <h3 class="pt-8 pb-5 font-bold text-2xl text-blue-950">
+            People First
+          </h3>
+          <p>
+            Our plans aren’t full of conditions and clauses to prevent payouts.
+            We make sure you’re covered when you need it.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
